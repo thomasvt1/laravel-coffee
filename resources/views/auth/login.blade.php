0b8b1365f1687fc02cca9ba1da3e4a0b19c391b2
BuildTools <unconfigured@null.spigotmc.org>
2018-03-06 22:56:38 +0100

    Remove redundant files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+    Hi!
+@endsection
+
+{{--
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -67,3 +72,4 @@
     </div>
 </div>
 @endsection
+--}}
