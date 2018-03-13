b983f5fd40a2a29e6da187c24d1bad290e4f5b06
Thomas van Vugt <0923458@hr.nl>
2018-03-13 11:11:04 +0100

    Add CSRF Token in Auth layout

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
+	{{-- CSRF token --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>{{ config('app.name', 'Laravel') }}</title>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/my-login.css') }}">
