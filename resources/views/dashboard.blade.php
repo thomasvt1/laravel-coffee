a0dbf6942a999b2c3e4acebdb43cc200aa66582a
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-04-05 16:47:20 +0200

    Fixed a card error

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,7 @@
             <option value="-">{{ $user->id }}</option>
         @endforeach
     </select>
+    <!--
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-4">
@@ -31,29 +32,25 @@
             </div>
         </div>
     </div>
-    
+    -->
     
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-4">
                 <div class="card ">
                     <div class="card-header ">
-                        <h4 class="card-title">Email Statistics</h4>
-                        <p class="card-category">Last Campaign Performance</p>
+                        <h4 class="card-title">Change preference</h4>
+                        <p class="card-category">Here it is possible to change your preference</p>
                     </div>
                     <div class="card-body ">
-                        <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
-                    </div>
+                        <select name="Locatie">
+                        <option selected hidden>Location</option>
+                        @foreach ($users as $user)    
+                                <option value="-">{{ $user->id }}</option>
+                            @endforeach
+                        </select>    
                     <div class="card-footer ">
-                        <div class="legend">
-                            <i class="fa fa-circle text-info"></i> Open
-                            <i class="fa fa-circle text-danger"></i> Bounce
-                            <i class="fa fa-circle text-warning"></i> Unsubscribe
-                        </div>
                         <hr>
-                        <div class="stats">
-                            <i class="fa fa-clock-o"></i> Campaign sent 2 days ago
-                        </div>
                     </div>
                 </div>
             </div>
