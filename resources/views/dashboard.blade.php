2224fadf8a42b65f4514fb7520f2310cc2b0f56c
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-04-06 17:47:36 +0200

    test to fix error

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,8 +15,8 @@
                     <div class="card-body ">
                         <select name="Locatie">
                             <option selected hidden>Location</option>
-                            @foreach ($machines as $machine)    
-                                <option value="-">{{ $machine->location }}</option>
+                            @foreach ($users as $user)    
+                                <option value="-">{{ $user->id }}</option>
                             @endforeach
                         </select>  
                         <select name="Cup">
