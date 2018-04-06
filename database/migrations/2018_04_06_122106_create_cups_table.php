74ea92aca3b97782d30f95155e1c2704a49f0ce5
Thomas van Vugt <thomasvvugt@gmail.com>
2018-04-06 12:22:04 +0200

    Re-added migrations

--- database/migrations/2018_04_06_122106_create_cups_table.php
+++ database/migrations/2018_04_06_122106_create_cups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDrinksTable extends Migration
+class CreateCupsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateDrinksTable extends Migration
      */
     public function up()
     {
-        Schema::create('drinks', function (Blueprint $table) {
+        Schema::create('cups', function (Blueprint $table) {
             $table->increments('id');
-			$table->string('name');
-			$table->integer('volume');
-			$table->data('data');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateDrinksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('drinks');
+        Schema::dropIfExists('cups');
     }
 }
