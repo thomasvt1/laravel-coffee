21fc33afa6a649f62184acf9ade37a1f8c1e13ba
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-04-09 20:49:59 +0200

    Updated file(s)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -41,7 +41,7 @@
                 </div>
             </div>
         </div>
-        <div class="row">
+        <!--<div class="row">
             <div class="col-md-6">
                 <div class="card ">
                     <div class="card-header ">
@@ -207,6 +207,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>-->
     </div>
 @endsection
