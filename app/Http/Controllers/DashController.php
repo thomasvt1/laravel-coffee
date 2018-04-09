7a56fa7c8abefe7957dd012f2dfbd03248794aef
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-04-09 16:58:00 +0200

    Update blade

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -46,7 +46,7 @@ class DashController extends Controller
     public function update(Request $request)
     {
         $cup = $request->input('cup');
-        return Request;
+        return $request;
         $preferences = \App\Preference::where('id', '=', 1)->update(array('drink_id' => 2));
         return DashController::index();        // Make sure you've got the Page model
         //if($preferences) {
