6e0937abc4ea9deeb4d889791dda23a5beed12e1
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-04-09 20:38:48 +0200

    Updated file(s)

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -24,15 +24,11 @@ class DashController extends Controller
      */
     public function index()
     {    
-   /*  App\SomeModel::whereIn('abc', ['a', 'b', 'c'])->get();
-    App\SomeModel::where('abc', 'a')->orWhere('abc', 'b')->orWhere('abc', 'c')->get();
-    DB::table('users')->where('name', 'John')->value('email');
-    DB::table('users')->where('name', 'John'); */
+
         $cups = \App\Cup::where('user_id', '=', Auth::user()->id);
         $users = \App\User::all();
         $machines = \App\Machine::all();
         $drinks = \App\Drink::all();
-        $cups_id = $cups->pluck('id');
         $preferences = \App\Preference::all()->whereIn('cup_id',$cups->pluck('id'));
         return view('dashboard', ['cups' => $cups, 'users' => $users, 'machines' => $machines, 'drinks' => $drinks, 'preferences'=>$preferences]);
 
@@ -42,13 +38,6 @@ class DashController extends Controller
     public function update(Request $request)
     {
         \App\Preference::where('id', '=', $request->input('cup'))->update(array('drink_id' => $request->input('drink')));
-        
-        //return $request->all();
         return DashController::index();        // Make sure you've got the Page model
-        //if($preferences) {
-        //$preferences->;
-       // $preferences->save();
-        //} 
-
     }
 }
