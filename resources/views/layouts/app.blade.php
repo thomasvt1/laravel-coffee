c94b5976d980fd714b52bffbcad1a41200f815e9
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-04-09 20:48:58 +0200

    Updated file(s)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -165,33 +165,13 @@
             <div class="container">
                 <nav>
                     <ul class="footer-menu">
-                        <li>
-                            <a href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Company
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Portfolio
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Blog
-                            </a>
-                        </li>
-                    </ul>
+                       
                     <p class="copyright text-center">
                         ©
                         <script>
                             document.write(new Date().getFullYear())
                         </script>
-                        <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+                         Magic Cow
                     </p>
                 </nav>
             </div>
