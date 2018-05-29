0cd56e1852adb81997f5635339cddbd92725d589
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-05-29 17:38:25 +0200

    Updated file(s)

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -50,7 +50,7 @@ class DashController extends Controller
 
             $preference_data['strength'] = $request->input('strength');
             \App\Preference::where('id', '=', $request->input('cup'))->update(array('drink_id' => $request->input('drink')));
-            \App\Preference::where('id', '=', $request->input('cup'))->update(array('data' => $preference_data));
+            \App\Preference::where('id', '=', $request->input('cup'))->update(array('data' =>json_encode($preference_data)));
             return back()->with('message', 'Preference updated successfully.');
         } else {
             return back()->with('message', "Your choice is too big for the selected cup. Cup volume: " . $cup_volume . ' drink volume: ' . $drink_volume . $request->input('strength') . '.');
