35e6630edf346154961830badbdaae58c75da523
Thomas van Vugt <thomasvvugt@gmail.com>
2018-05-29 10:00:43 +0200

    Edited id on API route

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('preference', function($id) {
+Route::get('preference/{id}', function($id) {
     return App\Preference::get($id);
 });
