1162ed10883dc6ad1d1de53ec8f6cf20a8590a0d
Thomas van Vugt <thomasvvugt@gmail.com>
2018-05-29 10:11:40 +0200

    Edited id on API route

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('preference/{id}', function($id) {
     $preference = App\Preference::find($id);
+    $preference_data = json_decode($preference->data);
     $drink = $preference->drink;
     $drink_data = json_decode($drink->data);
-    return $drink_data->id;
+    // Set id
+    $id = $drink_data->id;
+    $result = [ "K".$id."K".$id ];
+    // Append stength
+    if(isset($preference_data->strength)) {
+        array_push($result, "B00B00".$preference_data->strength);
+    }
+    // Start
+    array_push($result, "K34K34");
+    $final = [];
+    $final["serial"] = $result;
+    return json_encode($final);
 });
