b4aaee82ee75242d3d42b230b6f3c910ed8f3166
Thomas van Vugt <thomasvvugt@gmail.com>
2018-05-29 09:58:24 +0200

    Edited code style

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -23,32 +23,31 @@ class DashController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {    
+    {
 
         $cups = \App\Cup::where('user_id', '=', Auth::user()->id);
         $users = \App\User::all();
         $machines = \App\Machine::all();
         $drinks = \App\Drink::all();
-        $preferences = \App\Preference::all()->whereIn('cup_id',$cups->pluck('id'));
-        return view('dashboard', ['cups' => $cups, 'users' => $users, 'machines' => $machines, 'drinks' => $drinks, 'preferences'=>$preferences]);
+        $preferences = \App\Preference::all()->whereIn('cup_id', $cups->pluck('id'));
+        return view('dashboard', ['cups' => $cups, 'users' => $users, 'machines' => $machines, 'drinks' => $drinks, 'preferences' => $preferences]);
 
     }
-   
-    
+
+
     public function update(Request $request)
     {
         //of gekozen drank volume <= gekozen cup volume
         //$cup_volume = \App\Cup::where('cup_id', '=', $request->input('cup');
-        
+
         $cup_volume = \App\Cup::where('id', $request->input('cup'))->value('volume');
         $drink_volume = \App\Drink::where('id', $request->input('drink'))->value('volume');
-        if ($cup_volume >= $drink_volume){
-        //$request->input('cup') $request->input('drink')
-        \App\Preference::where('id', '=', $request->input('cup'))->update(array('drink_id' => $request->input('drink')));
-        return back()->with('message', 'Preference updated successfully.');
+        if ($cup_volume >= $drink_volume) {
+            //$request->input('cup') $request->input('drink')
+            \App\Preference::where('id', '=', $request->input('cup'))->update(array('drink_id' => $request->input('drink')));
+            return back()->with('message', 'Preference updated successfully.');
         } else {
-            return back()->with('message', "Your choice is too big for the selected cup. Cup volume: ".$cup_volume.' drink volume: '.$drink_volume.'.');
+            return back()->with('message', "Your choice is too big for the selected cup. Cup volume: " . $cup_volume . ' drink volume: ' . $drink_volume . '.');
         }
-        return DashController::index();        // Make sure you've got the Page model
     }
 }
