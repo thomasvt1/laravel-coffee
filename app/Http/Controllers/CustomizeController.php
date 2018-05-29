161f2dab0daa5f6f8a8bf3a95041877c0ad5bcf7
Thomas van Tilburg <thomasvt1@users.noreply.github.com>
2018-05-29 20:08:56 +0200

    Update CustomizeController.php

--- app/Http/Controllers/CustomizeController.php
+++ app/Http/Controllers/CustomizeController.php
@@ -45,8 +45,8 @@ class CustomizeController extends Controller
         if (strlen($newname) > 12 || strlen($newname) < 4)
             return back()->with('message', 'Please choose a name between 3 and 12 characters.');
         
-        else
-            \App\Cup::where('id', '=', $request->input('cup'))->where('user_id', '=', Auth::user()->id)->update(array('name' => $request->input('newname')));
+        
+        \App\Cup::where('id', '=', $request->input('cup'))->where('user_id', '=', Auth::user()->id)->update(array('name' => $request->input('newname')));
         
         return back()->with('message', 'Preference updated successfully.');
         
