31b98bc67bab2a0968688b597e3afd7639110ad7
Thomas van Tilburg <thomasvt1@users.noreply.github.com>
2018-05-29 19:31:54 +0200

    Update customize.blade.php

--- resources/views/customize.blade.php
+++ resources/views/customize.blade.php
@@ -17,10 +17,7 @@
                            
                         {{Form::open(['route' => 'updateName'])}}
                           
-                        {{ Form::select('location', $machines->pluck('location', 'id'), null, ['hidden', 'class' => 'form-control','placeholder' => 'Pick a location']) }}
                         {{ Form::select('cup', $user_cups->pluck('name', 'id'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a cup']) }}
-                        {{ Form::select('drink', $drinks->pluck('name', 'id'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a drink']) }}
-                        <p class="card-category">Choose your strength:</p>
                         {{ Form::input('range', 'strength') }}
 
                         <br>
@@ -41,11 +38,7 @@
                         <p class="card-category"></p>
                     </div>
                     <div class="card-body ">
-                        @foreach ($preferences as $preference)   
-                            <!--<p>{{ $preference}}</p>-->
-                             Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}}<br><br> 
-                        @endforeach
-                        
+                        Body
                     </div>
                 </div>
             </div>
