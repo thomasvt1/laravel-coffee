99cd67f3f0bc37b8297e8aba9a2598810b730677
Thomas van Tilburg <thomasvt1@users.noreply.github.com>
2018-05-29 20:03:32 +0200

    Update CustomizeController.php

--- app/Http/Controllers/CustomizeController.php
+++ app/Http/Controllers/CustomizeController.php
@@ -42,12 +42,7 @@ class CustomizeController extends Controller
 
         $newname = $request->input('newname');
         
-        $cups = \App\Cup::all();
-        $user_cups = $cups->where('user_id', '=', Auth::user()->id);
-        
-        return $user_cups;
-        
-        $user_cups->where('id', '=', $request->input('cup'))->update(array('name' => $request->input('newname')));
+        \App\Cup::where('id', '=', $request->where('user_id', '=', Auth::user()->id)->input('cup'))->update(array('name' => $request->input('newname')));
         
         
         return back()->with('message', 'Preference updated successfully.');
