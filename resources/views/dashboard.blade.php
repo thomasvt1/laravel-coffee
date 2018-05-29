c9628f4ce7b2f0f5b1fed431f13dd112c030568a
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-05-29 22:00:12 +0200

    Updated file(s)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,7 +43,8 @@
                     <div class="card-body ">
                         @foreach ($preferences as $preference)   
                             <!--<p>{{ $preference}}</p>-->
-                             Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}} {{json_decode($preference->data, true)}} <br><br> 
+                            $preference_data = json_decode($preference->data, true);
+                             Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}} {{$preference_data['strength']}} <br><br> 
                         @endforeach
                         
                     </div>
