09f35f223e78e722527ebc1be5bcd5404eb36493
Thomas van Tilburg <thomasvt1@users.noreply.github.com>
2018-05-29 16:36:25 +0200

    Create customize.blade.php

---
+++ resources/views/customize.blade.php
@@ -0,0 +1,221 @@
+<?php use App\Http\Controllers\DashController;?>
+@extends('layouts.app')
+
+@section('content')
+
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card ">
+                    <div class="card-header ">
+                        <h4 class="card-title">Change preference</h4>
+                        <p class="card-category">Here it is possible to change your preference</p>
+                    </div>
+                    <div class="card-body ">
+                           
+                        {{Form::open(['route' => 'updateName'])}}
+                          
+                        {{ Form::select('location', $machines->pluck('location', 'id'), null, ['hidden', 'class' => 'form-control','placeholder' => 'Pick a location']) }}
+                        {{ Form::select('cup', $cups->pluck('id', 'id'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a cup']) }}
+                        {{ Form::select('drink', $drinks->pluck('name', 'id'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a drink']) }}
+                        <p class="card-category">Choose your strength:</p>
+                        {{ Form::input('range', 'strength') }}
+
+                        <br>
+                        {{Form::submit('Submit')}}
+                        <br>
+                        {{Session::get('message')}}
+                        {{ Form::close() }}
+                        
+
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="card ">
+                    <div class="card-header ">
+                        <h4 class="card-title">Users preferences</h4>
+                        <p class="card-category"></p>
+                    </div>
+                    <div class="card-body ">
+                        @foreach ($preferences as $preference)   
+                            <!--<p>{{ $preference}}</p>-->
+                             Cup: {{$preference->cup_id}} preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}}<br><br> 
+                        @endforeach
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--<div class="row">
+            <div class="col-md-6">
+                <div class="card ">
+                    <div class="card-header ">
+                        <h4 class="card-title">2017 Sales</h4>
+                        <p class="card-category">All products including Taxes</p>
+                    </div>
+                    <div class="card-body ">
+                        <div id="chartActivity" class="ct-chart"></div>
+                    </div>
+                    <div class="card-footer ">
+                        <div class="legend">
+                            <i class="fa fa-circle text-info"></i> Tesla Model S
+                            <i class="fa fa-circle text-danger"></i> BMW 5 Series
+                        </div>
+                        <hr>
+                        <div class="stats">
+                            <i class="fa fa-check"></i> Data information certified
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card  card-tasks">
+                    <div class="card-header ">
+                        <h4 class="card-title">Tasks</h4>
+                        <p class="card-category">Backend development</p>
+                    </div>
+                    <div class="card-body ">
+                        <div class="table-full-width">
+                            <table class="table">
+                                <tbody>
+                                <tr>
+                                    <td>
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input class="form-check-input" type="checkbox" value="">
+                                                <span class="form-check-sign"></span>
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td>Sign contract for "What are conference organizers afraid of?"</td>
+                                    <td class="td-actions text-right">
+                                        <button type="button" rel="tooltip" title="Edit Task"
+                                                class="btn btn-info btn-simple btn-link">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                        <button type="button" rel="tooltip" title="Remove"
+                                                class="btn btn-danger btn-simple btn-link">
+                                            <i class="fa fa-times"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input class="form-check-input" type="checkbox" value="" checked>
+                                                <span class="form-check-sign"></span>
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td>Lines From Great Russian Literature? Or E-mails From My Boss?</td>
+                                    <td class="td-actions text-right">
+                                        <button type="button" rel="tooltip" title="Edit Task"
+                                                class="btn btn-info btn-simple btn-link">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                        <button type="button" rel="tooltip" title="Remove"
+                                                class="btn btn-danger btn-simple btn-link">
+                                            <i class="fa fa-times"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input class="form-check-input" type="checkbox" value="" checked>
+                                                <span class="form-check-sign"></span>
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td>Flooded: One year later, assessing what was lost and what was found when a
+                                        ravaging rain swept through metro Detroit
+                                    </td>
+                                    <td class="td-actions text-right">
+                                        <button type="button" rel="tooltip" title="Edit Task"
+                                                class="btn btn-info btn-simple btn-link">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                        <button type="button" rel="tooltip" title="Remove"
+                                                class="btn btn-danger btn-simple btn-link">
+                                            <i class="fa fa-times"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input class="form-check-input" type="checkbox" checked>
+                                                <span class="form-check-sign"></span>
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td>Create 4 Invisible User Experiences you Never Knew About</td>
+                                    <td class="td-actions text-right">
+                                        <button type="button" rel="tooltip" title="Edit Task"
+                                                class="btn btn-info btn-simple btn-link">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                        <button type="button" rel="tooltip" title="Remove"
+                                                class="btn btn-danger btn-simple btn-link">
+                                            <i class="fa fa-times"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input class="form-check-input" type="checkbox" value="">
+                                                <span class="form-check-sign"></span>
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td>Read "Following makes Medium better"</td>
+                                    <td class="td-actions text-right">
+                                        <button type="button" rel="tooltip" title="Edit Task"
+                                                class="btn btn-info btn-simple btn-link">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                        <button type="button" rel="tooltip" title="Remove"
+                                                class="btn btn-danger btn-simple btn-link">
+                                            <i class="fa fa-times"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                                <input class="form-check-input" type="checkbox" value="" disabled>
+                                                <span class="form-check-sign"></span>
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td>Unfollow 5 enemies from twitter</td>
+                                    <td class="td-actions text-right">
+                                        <button type="button" rel="tooltip" title="Edit Task"
+                                                class="btn btn-info btn-simple btn-link">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                        <button type="button" rel="tooltip" title="Remove"
+                                                class="btn btn-danger btn-simple btn-link">
+                                            <i class="fa fa-times"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>-->
+    </div>
+@endsection
