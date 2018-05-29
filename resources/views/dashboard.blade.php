b60c10244789e6f8a5d0e6ee421a4b52e1f52b47
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-05-29 19:12:20 +0200

    Updated file(s)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
                         {{Form::open(['route' => 'updateName'])}}
                           
                         {{ Form::select('location', $machines->pluck('location', 'id'), null, ['hidden', 'class' => 'form-control','placeholder' => 'Pick a location']) }}
-                        {{ Form::select('cup', $user_cups->pluck('name', 'name'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a cup']) }}
+                        {{ Form::select('cup', $user_cups->pluck('name', 'id'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a cup']) }}
                         {{ Form::select('drink', $drinks->pluck('name', 'id'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a drink']) }}
                         <p class="card-category">Choose your strength:</p>
                         {{ Form::input('range', 'strength') }}
