3d8f97dd8764b9dfcef2c4206e635ee518bc4b25
Thomas van Tilburg <thomasvt1@users.noreply.github.com>
2018-05-29 17:16:29 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,12 @@
             </div>
             <ul class="nav">
 	    	<!-- <li class="nav-item active"> -->
-		<li>
+		
+		@if (Route::currentRouteName() === 'Dashboard')
+			<li class="nav-item active">
+		@else
+			<li>
+		@endif
                     <a class="nav-link" href="/">
                         <i class="nc-icon nc-chart-pie-35"></i>
                         <p>Dashboard</p>                  
