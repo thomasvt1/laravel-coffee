76e866c1ad5afd5b17c58d0f45fb7abddf020d6e
Thomas van Tilburg <thomasvt1@users.noreply.github.com>
2018-05-29 19:22:42 +0200

    Update customize.blade.php

--- resources/views/customize.blade.php
+++ resources/views/customize.blade.php
@@ -39,7 +39,7 @@
             
             <div class="container-fluid">
         <div class="row">
-            <div class="col-md-5">
+            <div class="col-md-3">
                 <div class="card ">
                     <div class="card-header ">
                         <h4 class="card-title">Mug: Mugname</h4>
