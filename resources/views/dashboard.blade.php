3e501a0fa4d49dbabaed0347db83e8f0452ddef8
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-05-29 22:34:24 +0200

    Updated file(s)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -44,7 +44,7 @@
                         @foreach ($preferences as $preference)   
                             <!--<p>{{ $preference}}</p>-->
                            
-                             Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}} {{(json_decode(($preference->cup_id)->data,true))['strength']}} <br><br> 
+                             Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}} {{(json_decode(($preference->data)->data,true))['strength']}} <br><br> 
                         @endforeach
                         
                     </div>
