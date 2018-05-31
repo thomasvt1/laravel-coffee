6aaca8e26f61ee026114231757f7357935101864
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-05-31 20:00:11 +0200

    Updated file(s)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,10 +20,6 @@
                         {{ Form::select('location', $machines->pluck('location', 'id'), null, ['hidden', 'class' => 'form-control','placeholder' => 'Pick a location']) }}
                         {{ Form::select('cup', $user_cups->pluck('name', 'id'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a cup']) }}
                         {{ Form::select('drink', $drinks->pluck('name', 'id'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a drink']) }}
-                        {{ Form::select('startTime', $time, null, ['required', 'class' => 'form-control','placeholder' => 'Pick a start time']) }}
-                        {{ Form::select('EndTime', $time, null, ['required', 'class' => 'form-control','placeholder' => 'Pick a end time']) }}
-
-                        
                         <p class="card-category">Choose your strength:</p>
                         {{ Form::input('range', 'strength') }}
 
@@ -45,17 +41,11 @@
                         <p class="card-category"></p>
                     </div>
                     <div class="card-body ">
-                        <!--{{Form::open(['route' => 'deletePrefCup'])}}-->
                         @foreach ($preferences as $preference)   
                             <!--<p>{{ $preference}}</p>-->
                            
                              Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} -- preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}}  --  strength: {{(json_decode($preference->data,true))['strength']}} <br><br> 
                         @endforeach
-                        <br>
-                       <!-- {{Form::submit('Delete')}}
-                        <br>
-                        {{Session::get('message')}}
-                        {{ Form::close() }}-->
                         
                     </div>
                 </div>
