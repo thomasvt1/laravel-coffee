5b2a97dacde5207e52e6caee3716efd4e7f9e433
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-05-31 19:46:10 +0200

    Updated file(s)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                         <p class="card-category">Here it is possible to change your preference</p>
                     </div>
                     <div class="card-body ">
-                           
+                        <!--   
                         {{Form::open(['route' => 'updateCup'])}}
                         
                         {{ Form::select('location', $machines->pluck('location', 'id'), null, ['hidden', 'class' => 'form-control','placeholder' => 'Pick a location']) }}
@@ -22,7 +22,7 @@
                         {{ Form::select('drink', $drinks->pluck('name', 'id'), null, ['required', 'class' => 'form-control','placeholder' => 'Pick a drink']) }}
                       <!--  {{ Form::select('startTime', $time, null, ['required', 'class' => 'form-control','placeholder' => 'Pick a start time']) }}
                         {{ Form::select('EndTime', $time, null, ['required', 'class' => 'form-control','placeholder' => 'Pick a end time']) }}
--->
+
                         
                         <p class="card-category">Choose your strength:</p>
                         {{ Form::input('range', 'strength') }}
@@ -32,7 +32,7 @@
                         <br>
                         {{Session::get('message')}}
                         {{ Form::close() }}
-                        
+                        -->
 
                         
                     </div>
@@ -45,10 +45,10 @@
                         <p class="card-category"></p>
                     </div>
                     <div class="card-body ">
-                   <!-- {{Form::open(['route' => 'deletePref'])}}
+                   <!-- {{Form::open(['route' => 'deletePref'])}}-->
                         @foreach ($preferences as $preference)   
                             <!--<p>{{ $preference}}</p
-                             {{Form::checkbox('box', {{$cups->firstWhere('id', $preference->cup_id)->name}}, false)}}>
+                             {{Form::checkbox('box', {{$cups->firstWhere('id', $preference->cup_id)->name}}, false)}}>-->
                              Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} -- preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}}  --  strength: {{(json_decode($preference->data,true))['strength']}} <br><br> 
                         @endforeach
                       <!--  <br>
