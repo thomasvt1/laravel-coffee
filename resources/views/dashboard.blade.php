cca7bd0d9ad338e2af276ee4a7c8821ef6266b30
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-05-31 19:58:00 +0200

    Updated file(s)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -45,17 +45,17 @@
                         <p class="card-category"></p>
                     </div>
                     <div class="card-body ">
-                        {{Form::open(['route' => 'deletePrefCup'])}}
+                        <!--{{Form::open(['route' => 'deletePrefCup'])}}->>
                         @foreach ($preferences as $preference)   
                             <!--<p>{{ $preference}}</p>-->
                            
                              Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} -- preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}}  --  strength: {{(json_decode($preference->data,true))['strength']}} <br><br> 
                         @endforeach
                         <br>
-                        {{Form::submit('Delete')}}
+                       <!-- {{Form::submit('Delete')}}
                         <br>
                         {{Session::get('message')}}
-                        {{ Form::close() }}
+                        {{ Form::close() }}-->
                         
                     </div>
                 </div>
