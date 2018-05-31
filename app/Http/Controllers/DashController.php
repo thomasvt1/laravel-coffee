61a35fa98d43b0c88d9a0df1c14f3c1803ef1169
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-05-31 17:34:16 +0200

    Updated file(s)

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -32,7 +32,7 @@ class DashController extends Controller
         $machines = \App\Machine::all();
         $drinks = \App\Drink::all();
         $preferences = \App\Preference::all()->whereIn('cup_id', $user_cups->pluck('id'));
-        $time = [5,10,15]
+        $time = [5,10,15];
         return view('dashboard', ['user_cups' => $user_cups, 'cups' => $cups, 'users' => $users, 'machines' => $machines, 'drinks' => $drinks, 'preferences' => $preferences, 'time' => time]);
 
     }
