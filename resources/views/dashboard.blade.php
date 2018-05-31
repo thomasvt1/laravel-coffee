3e73bdca048dffe21394aec2122eb40ed06af01f
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-05-31 19:21:13 +0200

    Updated file(s)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -47,8 +47,8 @@
                     <div class="card-body ">
                     {{Form::open(['route' => 'deletePref'])}}
                         @foreach ($preferences as $preference)   
-                            <!--<p>{{ $preference}}</p>-->
-                             {{Form::checkbox('box', {{$cups->firstWhere('id', $preference->cup_id)->name}}, false)}}
+                            <!--<p>{{ $preference}}</p>
+                             {{Form::checkbox('box', {{$cups->firstWhere('id', $preference->cup_id)->name}}, false)}}-->
                              Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} -- preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}}  --  strength: {{(json_decode($preference->data,true))['strength']}} <br><br> 
                         @endforeach
                         <br>
