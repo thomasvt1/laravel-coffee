140562f2601d0a57ad9125ae0e9ee54123cd425b
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-06-01 11:10:18 +0200

    Updated file(s)

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -66,6 +66,7 @@ class DashController extends Controller
 
         $cup_volume = \App\Cup::where('id', $request->input('cup'))->value('volume');
         $drink_volume = \App\Drink::where('id', $request->input('drink'))->value('volume');
+        $preferences = \App\Preference::all()->whereIn('cup_id', $user_cups->pluck('id'));
         if ($cup_volume >= $drink_volume) {
             //$request->input('cup') $request->input('drink')
           
