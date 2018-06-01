625b1316c6784c2d8025cdcba5d850ad0b277854
Bryan Dijkgraaf <bryan@familie-dijkgraaf.nl>
2018-06-01 11:03:05 +0200

    Updated file(s)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -61,7 +61,7 @@
                         {{Form::open(['route' => 'deletePrefCup'])}}
                         @foreach ($preferences as $preference)   
                             <!--<p>{{ $preference}}</p>-->
-                            {{ Form::checkbox($preference->cup_id) }}
+                            {{ Form::checkbox('checkbox' . $preference->cup_id) }}
                             Cup: {{$cups->firstWhere('id', $preference->cup_id)->name}} -- preference drink: {{$drinks->firstWhere('id', $preference->drink_id)->name}}  --  strength: {{(json_decode($preference->data,true))['strength']}} <br><br> 
                         @endforeach
                         <br>
