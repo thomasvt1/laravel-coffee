e1851fff69ff538817f878bb1268295873b7e19e
Thomas van Tilburg <thomasvt1@users.noreply.github.com>
2018-06-02 11:13:59 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         
-        if(env('APP_ENV') == 'production') {
-            \URL::forceScheme('https');
-        }
+        \URL::forceScheme('https');
         
     }
 
