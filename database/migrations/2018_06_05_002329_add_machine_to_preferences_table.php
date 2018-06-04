fa84e83b9452f007a048f72a7160af879bc1d6d3
Thomas van Vugt <thomasvvugt@gmail.com>
2018-06-05 00:25:38 +0200

    Add machine to preference

---
+++ database/migrations/2018_06_05_002329_add_machine_to_preferences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMachineToPreferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('preferences', function (Blueprint $table) {
+            $table->unsignedInteger('machine_id');
+            $table->foreign('machine_id')->references('id')->on('machines');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('preferences', function (Blueprint $table) {
+            $table->dropForeign('preferences_machine_id_foreign');
+            $table->dropColumn('machine_id');
+        });
+    }
+}
