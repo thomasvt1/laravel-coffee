9a0b075f881f193ed6d37e53e67e8ccf6e64968b
BryanDijkgraaf <35568396+BryanDijkgraaf@users.noreply.github.com>
2018-06-19 15:01:04 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -80,6 +80,21 @@
                     </div>
                 </div>
             </div>
+           @foreach ($user_cups as $cup)
+            
+            <div class="col-md-2">
+                <div class="card ">
+                    <div class="card-header ">
+                        <h4 class="card-title">{{$cup->name}}</h4>
+                        <p class="card-category"></p>
+                    </div>
+                    <div class="card-body ">
+                        Cup content TODO
+                    </div>
+                </div>
+            </div>
+            
+            @endforeach
         </div>
     </div>
 @endsection
