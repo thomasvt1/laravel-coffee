15408e9ecbbf7d4ddd1053a78bf1d54b72a5e180
Thomas van Tilburg <thomasvt1@users.noreply.github.com>
2018-06-22 14:39:54 +0200

    Update customize.blade.php

--- resources/views/customize.blade.php
+++ resources/views/customize.blade.php
@@ -55,23 +55,20 @@
                     {{Session::get('message')}}
                     {{ Form::close() }}
                 </div>
-            </div>
-
-            
+            </div> 
         </div>
-        
         <div class="col-md-3">
                 @foreach ($user_cups as $cup)
                 <!--<div class="col-md-3"> -->
                 <div class="card ">
                     <div class="card-header ">
                         <h4 class="card-title">{{$cup->name}}</h4>
+                        <div class="card-body ">
+                        </div>
                     </div>
                 </div>
-
                 @endforeach
             </div>
-        
     </div>
 </div>
 @endsection
