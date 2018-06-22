fa779e5c3730fce4985d1dcf5f7ed241fce52eb9
BryanDijkgraaf <35568396+BryanDijkgraaf@users.noreply.github.com>
2018-06-22 14:11:06 +0200

    Update CustomizeController.php

--- app/Http/Controllers/CustomizeController.php
+++ app/Http/Controllers/CustomizeController.php
@@ -56,7 +56,7 @@ class CustomizeController extends Controller
     public function addCup(Request $request)
     {
         if($request->input('newuid')!= "" && $request->input('newuid') != ""){
-            \App\Cups::insert(['user_id' => Auth::user()->id, 'uid' => $request->input('newuid'), 'newname' => $request->input('newname')]);
+            \App\Cup::insert(['user_id' => Auth::user()->id, 'uid' => $request->input('newuid'), 'newname' => $request->input('newname')]);
         }
 
     }
