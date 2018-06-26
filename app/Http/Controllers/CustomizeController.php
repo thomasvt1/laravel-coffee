3a888889d7ab2c06bd210ba8abd68719f2f7654a
BryanDijkgraaf <35568396+BryanDijkgraaf@users.noreply.github.com>
2018-06-26 08:40:31 +0200

    Update CustomizeController.php

--- app/Http/Controllers/CustomizeController.php
+++ app/Http/Controllers/CustomizeController.php
@@ -56,7 +56,7 @@ class CustomizeController extends Controller
     public function addCup(Request $request)
     {
         if($request->input('newuid')!= "" && $request->input('newuid') != "" ){
-            if(strlen($request->input('newuid'))== 8 || strlen($request->input('newuid'))== 6 ){
+            if(strlen($request->input('newuid'))== 8 || strlen($request->input('newuid'))== 14 ){
                 \App\Cup::insert(['user_id' => Auth::user()->id, 'volume' => 400, 'uid' => $request->input('newuid'), 'name' => $request->input('cupname')]);
                 return back()->with('success', 'Preference updated successfully.');
             } else {
